9ccbf69dde97b10a33f32b0397614232f1c88155
anddyf <ferreiraanddy@gmail.com>
2019-11-24 01:53:08 -0500

    Still working on the Stretch goals

--- src/Nav.js
+++ src/Nav.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import {connect, store} from "./Store.js"
 const axios = require('axios')
 
-const Nav =  connect(({ page, location: { pathname }, match })=> {
+const Nav =  connect(({ page, location: { pathname }})=> {
 
     if(page.length === 0){
         return <div>...loading</div>
